b36d718458ca48daa581ad01d86ef4cf23b85fe1
Notenlish <71970100+Notenlish@users.noreply.github.com>
2023-08-03 16:38:55 +0300

    gallery & meta tags

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+const gallery = document.getElementsByClassName("section-gallery__content")[0];
+const leftBtn = document.getElementsByClassName("section-gallery-btn")[0];
+const rightBtn = document.getElementsByClassName("section-gallery-btn")[1];
+
+console.log(gallery);
+
+const rect = gallery.getBoundingClientRect();
+const galleryImages = gallery.getElementsByClassName("section-gallery__content-item");
+
+let moveX = ((rect.width - window.innerWidth) / 2) * -1;
+let scrollAmount = 0;
+let index = (galleryImages.length - 1) / 2;
+
+leftBtn.addEventListener("click", () => {
+  if (index === 0) return;
+  const elem = galleryImages[index];
+  scrollAmount += elem.getBoundingClientRect().width;
+  gallery.style.transform = `translateX( ${moveX + scrollAmount}px )`;
+  index--;
+});
+
+rightBtn.addEventListener("click", () => {
+  if (index === galleryImages.length - 1) return;
+  scrollAmount -= galleryImages[index].getBoundingClientRect().width;
+  gallery.style.transform = `translateX( ${moveX + scrollAmount}px )`;
+  index++;
+});
