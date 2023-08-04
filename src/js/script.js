ff0cd21a571ec23f37019a26af8b6593a3b827fd
Notenlish <71970100+Notenlish@users.noreply.github.com>
2023-08-04 16:58:39 +0300

    add some images

--- src/js/script.js
+++ src/js/script.js
@@ -5,33 +5,34 @@ const leftBtn = document.getElementsByClassName("section-gallery-btn")[0];
 const rightBtn = document.getElementsByClassName("section-gallery-btn")[1];
 const projectBtns = document.getElementsByClassName("section-projects__option");
 const projectBooks = document.getElementsByClassName("section-projects-book");
-const projectDocumentaries = document.getElementsByClassName(
-  "section-projects-documentary"
-);
+const projectDocumentaries = document.getElementsByClassName("section-projects-documentary");
 
-const rect = gallery.getBoundingClientRect();
-const galleryImages = gallery.getElementsByClassName(
-  "section-gallery__content-item"
-);
+setTimeout(function () {
+  const rect = gallery.getBoundingClientRect();
+  const galleryImages = gallery.getElementsByClassName("section-gallery__content-item");
 
-let moveX = ((rect.width - window.innerWidth) / 2) * -1;
-let scrollAmount = 0;
-let index = (galleryImages.length - 1) / 2;
-
-leftBtn.addEventListener("click", () => {
-  if (index === 0) return;
-  const elem = galleryImages[index];
-  scrollAmount += elem.getBoundingClientRect().width;
+  let moveX = ((rect.width - window.innerWidth) / 2) * -1;
+  let scrollAmount = 0;
+  let index = (galleryImages.length - 1) / 2;
   gallery.style.transform = `translateX( ${moveX + scrollAmount}px )`;
-  index--;
-});
 
-rightBtn.addEventListener("click", () => {
-  if (index === galleryImages.length - 1) return;
-  scrollAmount -= galleryImages[index].getBoundingClientRect().width;
-  gallery.style.transform = `translateX( ${moveX + scrollAmount}px )`;
-  index++;
-});
+  const stopAmount = 1;
+
+  leftBtn.addEventListener("click", () => {
+    if (index === stopAmount) return;
+    const elem = galleryImages[index];
+    scrollAmount += elem.getBoundingClientRect().width;
+    gallery.style.transform = `translateX( ${moveX + scrollAmount}px )`;
+    index--;
+  });
+
+  rightBtn.addEventListener("click", () => {
+    if (index === galleryImages.length - (1 + stopAmount)) return;
+    scrollAmount -= galleryImages[index].getBoundingClientRect().width;
+    gallery.style.transform = `translateX( ${moveX + scrollAmount}px )`;
+    index++;
+  });
+}, 500);
 
 projectBtns[0].addEventListener("click", () => {
   for (const element of projectBooks) {
